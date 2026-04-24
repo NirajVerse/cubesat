a2f411d92e1dbdf91617dbee563d6c7e433d535a
Niraj Gupta <ng739@muscadine-node-1.hpc.msstate.edu>
2026-04-23 23:23:03 -0500

    increased epoch and trained

--- yolo_model/config/yolo_config.py
+++ yolo_model/config/yolo_config.py
@@ -11,7 +11,7 @@ MODEL_NAME = 'yolo26n'  # yolov11n (nano), yolov11s (small), yolov11m (medium), 
 DEVICE = 'cuda'  # 'cuda' or 'cpu'
 
 # Training Configuration (for future use)
-EPOCHS = 100
+EPOCHS = 200
 BATCH_SIZE = 32
 IMG_SIZE = 512
 IMGSZ = 512
