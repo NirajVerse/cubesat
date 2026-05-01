21255187fe5948c2d65e25dc77c7e12829f9d15d
NirajVerse <137820145+NirajVerse@users.noreply.github.com>
2026-05-01 12:50:43 -0500

    tuned infer confi

--- yolo_model/config/yolo_config.py
+++ yolo_model/config/yolo_config.py
@@ -29,8 +29,8 @@ TEST_IMG_DIR = os.path.join(DATASET_PATH, 'images/test')
 
 # Inference
 
-INFERENCE_CONF = 0.3  # Confidence threshold for predictions
-INFERENCE_IOU = 0.1  # NMS IOU threshold
+INFERENCE_CONF = 0.2  # Confidence threshold for predictions
+INFERENCE_IOU = 0.2  # NMS IOU threshold
 
 # Visualization
 SAVE_PREDICTIONS = True
